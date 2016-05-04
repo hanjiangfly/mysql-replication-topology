26d0e0bf5685cd3da07ed01b2580d928d40df032
hanjiangfly <hanjiangfly@hotmail.com>
2016-05-04 16:31:05 +0800

    begin version 0.0.0.1

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+@version: ??
+@author: jiang.han
+@license: Apache Licence
+@software: PyCharm
+@file: main.py
+@time: 2016/5/4 16:24
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
